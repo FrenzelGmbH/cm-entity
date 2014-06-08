38a4b2ccc77d777e23043440bc4f570ce3bd2d62
Philipp Frenzel <philipp@frenzel.net>
2014-06-08 11:40:35 +0200

    change form

--- views/entity/_form_crud.php
+++ views/entity/_form_crud.php
@@ -14,21 +14,11 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'name')->textInput(['maxlength' => 140]) ?>
-
-    <?= $form->field($model, 'prename')->textInput(['maxlength' => 100]) ?>
-
-    <?= $form->field($model, 'name_two')->textInput(['maxlength' => 100]) ?>
-
-    <?= $form->field($model, 'name_three')->textInput(['maxlength' => 100]) ?>
-
     <?= $form->field($model, 'param_date')->textInput() ?>
 
     <?= $form->field($model, 'param_text')->textarea(['rows' => 6]) ?>
 
-    <?= $form->field($model, 'official_one')->textInput(['maxlength' => 60]) ?>
-
-    <?= $form->field($model, 'official_two')->textInput(['maxlength' => 60]) ?>
+    <?= $form->field($model, 'user_id')->textInput() ?>
 
     <?= $form->field($model, 'mod_id')->textInput() ?>
 
@@ -41,14 +31,27 @@ use yii\widgets\ActiveForm;
     <?= $form->field($model, 'deleted_at')->textInput() ?>
 
     <?= $form->field($model, 'entity_type_id')->textInput() ?>
-    
+
+    <?= $form->field($model, 'entity_relation_id')->textInput() ?>
+
+    <?= $form->field($model, 'name')->textInput(['maxlength' => 140]) ?>
+
+    <?= $form->field($model, 'prename')->textInput(['maxlength' => 100]) ?>
+
+    <?= $form->field($model, 'name_two')->textInput(['maxlength' => 100]) ?>
+
+    <?= $form->field($model, 'name_three')->textInput(['maxlength' => 100]) ?>
+
     <?= $form->field($model, 'mod_table')->textInput(['maxlength' => 100]) ?>
 
     <?= $form->field($model, 'system_key')->textInput(['maxlength' => 100]) ?>
 
     <?= $form->field($model, 'system_name')->textInput(['maxlength' => 100]) ?>
 
-    
+    <?= $form->field($model, 'official_one')->textInput(['maxlength' => 60]) ?>
+
+    <?= $form->field($model, 'official_two')->textInput(['maxlength' => 60]) ?>
+
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? Yii::t('app', 'Create') : Yii::t('app', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
     </div>
